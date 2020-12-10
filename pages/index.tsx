05e6f19c10207491f96edef132f1e8d322d6f67f
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-11 00:31:04 +0900

    show image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <Link href="test1">検証1</Link>
-    <Link href="test2">検証2</Link>
-    <Link href="test3">検証3</Link>
+  <Layout>
+    <img src="/sample.png" width="270" height="480" />
+    <Image src="/sample.png" width="270" height="480" />
   </Layout>
 );
 
